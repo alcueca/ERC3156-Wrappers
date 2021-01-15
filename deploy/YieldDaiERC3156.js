5b1483336d5aa83d825a3b6ecb049ee1b4aaf446
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-01-15 15:33:03 +0000

    removed redundant addresses file

--- deploy/YieldDaiERC3156.js
+++ deploy/YieldDaiERC3156.js
@@ -1,14 +1,3 @@
-const addresses = {
-  '1' : {
-      'fyDaiLP21Mar31': '0xb39221E6790Ae8360B7E8C1c7221900fad9397f9',
-      'YieldMathWrapper': '0xfcb06dce37a98081900fac325255d92dff94a107',
-  },
-  '42' : {
-      'fyDaiLP21Mar31': '0x08cc239a994A10118CfdeEa9B849C9c674C093d3',
-      'YieldMathWrapper': '0x032994fd2282fde1b7eb0893db6ca9a592ae99c7',
-  }
-}
-
 const func = async function ({ deployments, getNamedAccounts, getChainId }) {
   const { deploy, read, execute } = deployments;
   const { deployer } = await getNamedAccounts();
