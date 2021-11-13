438bd55c02edcb85e9df3114f6f722fbae975fb7
yashnaman <shahyashs145@gmail.com>
2021-11-13 09:53:41 +0530

    fix: fix compilation error

--- contracts/uniswapV3/UniswapV3ERC3156.sol
+++ contracts/uniswapV3/UniswapV3ERC3156.sol
@@ -4,31 +4,31 @@
 pragma solidity ^0.7.6;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "erc3156/contracts/interfaces/IERC3156FlashBorrower.sol";
-import "erc3156/contracts/interfaces/IERC3156FlashLender.sol";
-import "@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol";
-import "@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol";
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import 'erc3156/contracts/interfaces/IERC3156FlashBorrower.sol';
+import 'erc3156/contracts/interfaces/IERC3156FlashLender.sol';
+import '@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol';
+import '@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol';
 
-import "@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol";
-import "@uniswap/v3-periphery/contracts/base/PeripheryImmutableState.sol";
-import "@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol";
-import "@uniswap/v3-periphery/contracts/libraries/CallbackValidation.sol";
-import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import '@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol';
+import '@uniswap/v3-periphery/contracts/base/PeripheryImmutableState.sol';
+import '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol';
+import '@uniswap/v3-periphery/contracts/libraries/CallbackValidation.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
-import "@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol";
-import "@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
+import '@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol';
+import '@uniswap/v3-periphery/contracts/base/PeripheryPayments.sol';
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract UniswapV3ERC3156 is IERC3156FlashLender, IUniswapV3FlashCallback {
     using SafeMath for uint256;
 
     // CONSTANTS
-    bytes32 public constant CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
+    bytes32 public constant CALLBACK_SUCCESS = keccak256('ERC3156FlashBorrower.onFlashLoan');
     IUniswapV3Factory public factory;
 
     // ACCESS CONTROL
@@ -47,7 +47,11 @@ contract UniswapV3ERC3156 is IERC3156FlashLender, IUniswapV3FlashCallback {
     /// @param factory_ Uniswap v3 UniswapV3Factory address
     /// @param weth_ Weth contract used in Uniswap v3 Pairs
     /// @param usdl_ Usdl contract used in Uniswap v3 Pairs
-    constructor(IUniswapV3Factory factory_, address weth_, address usdl_) {
+    constructor(
+        IUniswapV3Factory factory_,
+        address weth_,
+        address usdl_
+    ) {
         factory = factory_;
         weth = weth_;
         usdl = usdl_;
@@ -84,10 +88,10 @@ contract UniswapV3ERC3156 is IERC3156FlashLender, IUniswapV3FlashCallback {
      * @return The amount of `token` to be charged for the loan, on top of the returned principal.
      */
     function flashFee(address token, uint256 amount) public view override returns (uint256) {
-        require(getPairAddress(token) != address(0), "Unsupported currency");
-        uint256 wLoan = amount * 1e6 / (1e6 - 3000); // 3000 = lpFees
-        uint256 wOwed = w_loan * 1e6 / (1e6 - 3000); // 3000 = loanFees
-        uint256 fee = w_owed - w_loan;
+        require(getPairAddress(token) != address(0), 'Unsupported currency');
+        uint256 wLoan = (amount * 1e6) / (1e6 - 3000); // 3000 = lpFees
+        uint256 wOwed = (wLoan * 1e6) / (1e6 - 3000); // 3000 = loanFees
+        uint256 fee = wOwed - wLoan;
         return fee;
     }
 
@@ -98,24 +102,24 @@ contract UniswapV3ERC3156 is IERC3156FlashLender, IUniswapV3FlashCallback {
      * @param amountOfCollateralToBorrow The amount of tokens lent.
      * @param userData A data parameter to be passed on to the `receiver` for any custom use.
      */
-    function flashLoan(IERC3156FlashBorrower receiver, address token, uint256 amountOfCollateralToBorrow, bytes memory userData) external override returns(bool) {
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token,
+        uint256 amountOfCollateralToBorrow,
+        bytes memory userData
+    ) external override returns (bool) {
         address pairAddress = getPairAddress(token);
-        require(pairAddress != address(0), "Unsupported currency");
+        require(pairAddress != address(0), 'Unsupported currency');
 
         if (permissionedPairAddress != pairAddress) permissionedPairAddress = pairAddress; // access control
 
-        ( uint256 amountOfUSDLToMint, address poolForWETHLoan ) = abi.decode(userData, (uint256, address)); // Use this to unpack arbitrary data
+        (uint256 amountOfUSDLToMint, address poolForWETHLoan) = abi.decode(userData, (uint256, address)); // Use this to unpack arbitrary data
 
         IUniswapV3Pool(poolForWETHLoan).flash(
             address(receiver),
             0,
             amountOfCollateralToBorrow,
-            abi.encode(
-                amountOfUSDLToMint,
-                msg.sender,
-                receiver,
-                token
-            )
+            abi.encode(amountOfUSDLToMint, msg.sender, receiver, token)
         );
         return true;
     }
@@ -127,31 +131,20 @@ contract UniswapV3ERC3156 is IERC3156FlashLender, IUniswapV3FlashCallback {
         bytes calldata data
     ) external override {
         // access control
-        require(msg.sender == permissionedPairAddress, "only permissioned UniswapV3 pair can call");
+        require(msg.sender == permissionedPairAddress, 'only permissioned UniswapV3 pair can call');
 
-        (uint256 amount, IERC3156FlashBorrower receiver, address origin, address token) = abi.decode(data, (uint256, IERC3156FlashBorrower, address, address));
+        (uint256 amount, IERC3156FlashBorrower receiver, address origin, address token) = abi.decode(
+            data,
+            (uint256, IERC3156FlashBorrower, address, address)
+        );
         uint256 fee = flashFee(token, amount);
 
-        bytes memory decoded = abi.encode(
-            FlashCallbackData({
-                amountOfUSDLToMint: amount,
-                poolForWETHLoan: msg.sender
-            })
-        );
+        bytes memory decoded = abi.encode(FlashCallbackData({amountOfUSDLToMint: amount, poolForWETHLoan: msg.sender}));
 
         // // send the borrowed amount to the receiver
         IERC20(token).transfer(address(receiver), amount);
         // // do whatever the user wants
-        require(
-            receiver.onFlashLoan(
-                origin, 
-                token, 
-                amount, 
-                fee,
-                decoded
-            ) == CALLBACK_SUCCESS,
-            "Callback failed"
-        );
+        require(receiver.onFlashLoan(origin, token, amount, fee, decoded) == CALLBACK_SUCCESS, 'Callback failed');
         // // // retrieve the borrowed amount plus fee from the receiver and send it to the uniswap pair
         IERC20(token).transferFrom(address(receiver), msg.sender, amount.add(fee));
     }
